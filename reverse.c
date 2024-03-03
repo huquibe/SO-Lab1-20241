c4cb828a4d548221495f78bba245081db7d3610d
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2024-03-03 11:28:52 -0500

    handling "too many arguments"

---
+++ reverse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv)
+{
+    if (argc > 3)
+    {
+        fprintf(stderr, "usage: reverse <input> <output>\n");
+        exit(1);
+    }
+
+    // Funcionalidad para reverse
+    return 0;
+}
