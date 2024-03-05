1c36eac5d2cc7488cf5e28a5ea5b3827e14e03ab
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2024-03-05 18:18:18 -0500

    handling "same name files"

--- reverse.c
+++ reverse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -23,8 +24,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if (input_file == output_file)
-    {
+    if (strcmp(argv[1], argv[2]) == 0) {
         fprintf(stderr, "reverse: input and output file must differ\n");
         exit(1);
     }
