e4c6b4f5afe0c791ec5ec057360cfcd340b03598
Tatiana Sanchez <tatiana.sanchez.sanin@gmail.com>
2024-03-05 18:20:23 -0500

    handling "files hard linked"

--- reverse.c
+++ reverse.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int areFilesHardlinked(const char *file1, const char *file2)
+{
+    struct stat stat1, stat2;
+    if (stat(file1, &stat1) != 0 || stat(file2, &stat2) != 0)
+        return -1; // Error occurred
+
+    return (stat1.st_dev == stat2.st_dev && stat1.st_ino == stat2.st_ino);
+}
 
 int main(int argc, char *argv[])
 {
@@ -24,7 +36,14 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if (strcmp(argv[1], argv[2]) == 0) {
+    if (strcmp(argv[1], argv[2]) == 0)
+    {
+        fprintf(stderr, "reverse: input and output file must differ\n");
+        exit(1);
+    }
+
+    if (areFilesHardlinked(argv[1], argv[2]))
+    {
         fprintf(stderr, "reverse: input and output file must differ\n");
         exit(1);
     }
